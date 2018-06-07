25cf8c5f1ed53132825f93cae558b65761e5b960
AkhilKagool <AkhilKagool@github.com>
2018-06-07 13:19:10 +0530

    new emp table created

---
+++ AKHIL_TEST/Tables/EMP.sql
@@ -0,0 +1,8 @@
+CREATE TABLE akhil_test.emp (
+  empno NUMBER(5) NOT NULL,
+  ename VARCHAR2(15 BYTE) NOT NULL,
+  "JOB" VARCHAR2(10 BYTE),
+  hiredate DATE,
+  deptname VARCHAR2(10 BYTE) NOT NULL,
+  PRIMARY KEY (empno)
+);
