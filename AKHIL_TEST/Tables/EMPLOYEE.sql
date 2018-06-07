a4c011a26e2d3826ea3a0be050709b08964ecd0d
AkhilKagool <AkhilKagool@github.com>
2018-06-07 17:42:02 +0530

    employee table dropped

--- AKHIL_TEST/Tables/EMPLOYEE.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE akhil_test.employee (
-  "NAME" VARCHAR2(100 BYTE),
-  "ID" NUMBER(3),
-  city VARCHAR2(100 BYTE),
-  subject VARCHAR2(100 BYTE),
-  dept VARCHAR2(100 BYTE),
-  names VARCHAR2(255 BYTE),
-  pen VARCHAR2(255 BYTE)
-);
